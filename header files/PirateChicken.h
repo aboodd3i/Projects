a2d3be2e455bd78c86f4cf81d4c06aba7b5119b0
msaadsohail05 <msaad.bahria@gmail.com>
2025-05-11 23:13:12 +0500

    Update PirateChicken.h

--- header files/PirateChicken.h
+++ header files/PirateChicken.h
@@ -5,20 +5,20 @@
 #include <iostream>
 
 class PirateChicken : public Enemy {
-    private:
-        int hitPoints;
-        
-    public:
-        PirateChicken(float x, float y) : Enemy(x, y), hitPoints(2) {
-            if (!texture.loadFromFile("D:/My Documents/OOP/Project/images/piratechicken.png")) {
-                std::cout << "Failed to load enemy image!" << std::endl;
-            }
-           
+private:
+    int hitPoints;
+
+public:
+    PirateChicken(float x, float y) : Enemy(x, y), hitPoints(2) {
+        if (!texture.loadFromFile("D:/My Documents/OOP/Project/images/piratechicken.png")) {
+            std::cout << "Failed to load enemy image!" << std::endl;
         }
 
-        //checking for collision with a bullet
-        bool takeHit() {
-            hitPoints--;
-            return hitPoints <= 0; // true when 2 bullets are hit
-        }
+    }
+
+    //checking for collision with a bullet
+    bool takeHit() {
+        hitPoints--;
+        return hitPoints <= 0; // true when 2 bullets are hit
+    }
 };
